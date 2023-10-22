c2e6f8173d3314a78eb8d92323af68bafb8d5051
RmnZbr <zabirov_@mail.ru>
2023-10-23 00:42:34 +0300

    AdvancedListArray

--- Task2/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        AdvancedListArray list = new AdvancedListArray();
-        for (int i = 0; i < 100; i++) {
-            list.add(i);
-        }
-    }
-}
