59c93ee6e3cb9b95c8fa14e49f4292d62424a605
RmnZbr <zabirov_@mail.ru>
2023-10-25 23:27:50 +0300

    MaxSumOfAscendingNumbers

---
+++ Task2/src/Task3/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    int[] array = {1, 3, 8, 2, 1, 9, 11, 4, 7, 17};
+
+    public static void main(String[] args) {
+        Main test = new Main();
+        test.maxSumOfAscendingNumbers(test.array);
+    }
+
+    public int maxSumOfAscendingNumbers(int[] array) {
+        int maxSum = 0;
+        int currentMaxSum = array[0];
+        for(int i = 0; i < array.length - 1; i++) {
+            if(array[i] < array[i + 1]) {
+                currentMaxSum = currentMaxSum + array[i + 1];
+            } else {
+                if(currentMaxSum > maxSum) {
+                    maxSum = currentMaxSum;
+                }
+                currentMaxSum = array[i + 1];
+            }
+        }
+        if(currentMaxSum > maxSum) {
+            maxSum = currentMaxSum;
+        }
+        return maxSum;
+    }
+
+}
