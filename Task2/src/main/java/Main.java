397f378b34eed45e3501985ccbf25d6f9b55b76a
RmnZbr <zabirov_@mail.ru>
2023-11-10 08:03:07 +0300

    SimpleHashMap

--- Task2/src/main/java/Main.java
+++ Task2/src/main/java/Main.java
@@ -4,25 +4,5 @@ public class Main {
 
         SimpleHashMap map = new SimpleHashMap();
 
-        map.put("privet", 0);
-        map.put("privet1", "one");
-        map.put("privet2", 2);
-        map.put("privet3", 3d);
-        map.put("privet4", 4f);
-        map.put("privet5", 5L);
-        map.put("privet6", false);
-        map.put("123", 123);
-        map.put("777777", 777);
-
-        System.out.println(map.get("privet"));
-        System.out.println(map.get("privet1"));
-        System.out.println(map.get("privet2"));
-        System.out.println(map.get("privet3"));
-        System.out.println(map.get("privet4"));
-        System.out.println(map.get("privet5"));
-        System.out.println(map.get("privet6"));
-        System.out.println(map.get("privet7"));
-        System.out.println(map.get("123"));
-        System.out.println(map.get("777777"));
     }
 }
--- Task2/src/main/java/SimpleHashMap.java
+++ Task2/src/main/java/SimpleHashMap.java
@@ -1,18 +1,112 @@
 public class SimpleHashMap implements SimpleMap {
 
-    AdvancedArray array;
+    private static final int DEFAULT_SIZE = 16;
+    private static final double LOAD_FACTOR = 0.75;
+
+    private int size;
+    private Node[] array;
+
+    private static class Node {
+
+        private Object value;
+        private String key;
+        private Node next;
+
+        private Node(String key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (obj instanceof Node) {
+                return ((Node) obj).key.equals(key);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.key.hashCode();
+        }
+    }
 
     public SimpleHashMap() {
-        this.array = new AdvancedArray();
+        this.array = new Node[DEFAULT_SIZE];
+    }
+
+    private int getIndex(String key) {
+        int index = key.hashCode() % array.length;
+        if (index >= 0) {
+            return index;
+        }
+        return -index;
+    }
+    private int getIndexForNewArray(String key) {
+        int index = key.hashCode() % (array.length * 2);
+        if (index >= 0) {
+            return index;
+        }
+        return -index;
+    }
+    private void grow() {
+        if (size >= array.length * LOAD_FACTOR) {
+            Node[] newArray = new Node[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                Node element = array[i];
+                while (element != null) {
+                    int newIndex = getIndexForNewArray(element.key);
+                    if (newArray[newIndex] == null) {
+                        newArray[newIndex] = new Node(element.key, element.value);
+                    } else {
+                        newArray[newIndex].next = newArray[newIndex];
+                        newArray[newIndex].value = element.value;
+                        newArray[newIndex].key = element.key;
+                    }
+                    element = element.next;
+                }
+            }
+            array = newArray;
+        }
+    }
+    private Node searchByNodes (String key, int index) {
+        int keyHashCode = key.hashCode();
+        Node head = array[index];
+        while (head != null) {
+            if (head.hashCode() == keyHashCode && head.key.equals(key)) {
+                return head;
+            }
+            head = head.next;
+        }
+        return null;
     }
 
     @Override
     public void put(String key, Object value) {
-        array.put(key, value);
+        int index = getIndex(key);
+        if (array[index] == null) {
+            array[index] = new Node(key, value);
+            size++;
+            grow();
+        } else {
+            if (searchByNodes(key, index) == null) {
+                Node node = new Node(key, value);
+                node.next = array[index];
+                array[index] = node;
+            } else {
+                searchByNodes(key, index).value = value;
+            }
+        }
     }
-
     @Override
     public Object get(String key) {
-        return array.get(key);
+        if (searchByNodes(key, getIndex(key)) == null) {
+            return null;
+        } else {
+            return searchByNodes(key, getIndex(key)).value;
+        }
     }
 }
